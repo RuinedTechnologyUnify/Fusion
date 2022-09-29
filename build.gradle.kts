fb8aed90243b6b05a430a06e56de1c317c714610
IPECTER 이팩터 <80433772+IPECTER@users.noreply.github.com>
2022-09-29 22:41:46 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,16 @@
 import io.papermc.paperweight.util.constants.PAPERCLIP_CONFIG
-
-val paperMavenPublicUrl = "https://papermc.io/repo/repository/maven-public/"
+import java.nio.charset.StandardCharsets
 
 plugins {
     java
-    `maven-publish`
     id("com.github.johnrengelman.shadow") version "7.1.2" apply false
     id("io.papermc.paperweight.patcher") version "1.3.8"
 }
 
 repositories {
     mavenCentral()
-    maven(paperMavenPublicUrl) {
-        content {
-            onlyForConfigurations(PAPERCLIP_CONFIG)
-        }
+    maven("https://papermc.io/repo/repository/maven-public/") {
+        content { onlyForConfigurations(PAPERCLIP_CONFIG) }
     }
 }
 
@@ -24,40 +20,29 @@ dependencies {
     paperclip("io.papermc:paperclip:3.0.2")
 }
 
-allprojects {
+subprojects {
     apply(plugin = "java")
-    apply(plugin = "maven-publish")
 
     java {
         toolchain {
             languageVersion.set(JavaLanguageVersion.of(17))
         }
     }
-}
 
-subprojects {
     tasks.withType<JavaCompile>().configureEach {
-        options.encoding = Charsets.UTF_8.name()
+        options.encoding = StandardCharsets.UTF_8.name()
         options.release.set(17)
     }
 
-    tasks.withType<Javadoc> {
-        options.encoding = Charsets.UTF_8.name()
-    }
-
-    tasks.withType<ProcessResources> {
-        filteringCharset = Charsets.UTF_8.name()
-    }
-
     repositories {
+        mavenLocal()
         mavenCentral()
-        maven(paperMavenPublicUrl)
         maven("https://oss.sonatype.org/content/groups/public/")
+        maven("https://papermc.io/repo/repository/maven-public/")
         maven("https://ci.emc.gs/nexus/content/groups/aikar/")
         maven("https://repo.aikar.co/content/groups/aikar")
         maven("https://repo.md-5.net/content/repositories/releases/")
         maven("https://hub.spigotmc.org/nexus/content/groups/public/")
-        maven("https://oss.sonatype.org/content/repositories/snapshots/")
         maven("https://jitpack.io")
     }
 }
@@ -65,15 +50,21 @@ subprojects {
 paperweight {
     serverProject.set(project(":Fusion-Server"))
 
-    remapRepo.set(paperMavenPublicUrl)
-    decompileRepo.set(paperMavenPublicUrl)
+    remapRepo.set("https://maven.fabricmc.net/")
+    decompileRepo.set("https://files.minecraftforge.net/maven/")
 
-    usePaperUpstream(providers.gradleProperty("paperCommit")) {
-        withPaperPatcher {
+    useStandardUpstream("purpur") {
+        url.set(github("PurpurMC", "Purpur"))
+        ref.set(providers.gradleProperty("purpurRef"))
+
+        withStandardPatcher {
+            apiSourceDirPath.set("Purpur-API")
+            serverSourceDirPath.set("Purpur-Server")
+
             apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
-            apiOutputDir.set(layout.projectDirectory.dir("Fusion-API"))
-
             serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
+
+            apiOutputDir.set(layout.projectDirectory.dir("Fusion-API"))
             serverOutputDir.set(layout.projectDirectory.dir("Fusion-Server"))
         }
     }
