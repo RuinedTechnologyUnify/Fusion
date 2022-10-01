ca55234d44e35da0b92b70daec2404e8a8c9124d
IPECTER <ipectert@gmail.com>
2022-10-01 15:42:18 +0900

    [ Prepare ] UpstreamCommit.sh

--- scripts/UpstreamCommit.sh
+++ scripts/UpstreamCommit.sh
@@ -3,19 +3,24 @@
 # requires curl & jq
 # upstreamCommit
 
-set -e
 current=$(cat ../gradle.properties | grep purpurRef | sed 's/purpurRef = //')
+upstream=$(git ls-remote https://github.com/PurpurMC/Purpur | grep ver/1.19.2 | cut -f 1)
 
-purpur=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/PurpurMC/Purpur/compare/$current...HEAD | jq -r '.commits[] | "PurpurMC/Purpur@\(.sha[:7]) \(.commit.message | split("\r\n")[0] | split("\n")[0])"')
+purpur=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/PurpurMC/Purpur/compare/$current...$upstream | jq -r '.commits[] | "PurpurMC/Purpur@\(.sha[:7]) \(.commit.message | split("\r\n")[0] | split("\n")[0])"')
 
 updated=""
 logsuffix=""
+
 if [ ! -z "purpur" ]; then
     logsuffix="$logsuffix\n\nPurpur Changes:\n$purpur"
     updated="Purpur"
 fi
+
 disclaimer="Upstream has released updates that appear to apply and compile correctly"
 
 log="${UP_LOG_PREFIX}Updated Upstream ($updated)\n\n${disclaimer}${logsuffix}"
-sed -i 's/purpurRef = .*/purpurRef = '"$current"'/' ../gradle.properties
+
+sed -i 's/purpurRef = .*/purpurRef = '"$upstream"'/' ../gradle.properties
+
+git add ../gradle.properties
 echo -e "$log" | git commit -F -
