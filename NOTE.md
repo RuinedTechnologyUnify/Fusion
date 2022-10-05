415c838fb3a0c905b559babcd25e46c101dbb24c
IPECTER <ipectert@gmail.com>
2022-10-05 22:53:30 +0900

    NMS-Test

---
+++ NOTE.md
@@ -0,0 +1,2 @@
+#NMS
+compileOnly(files("../.gradle/caches/paperweight/upstreams/paper/.gradle/caches/paperweight/taskCache/spigotRemapJar.jar"))
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,4 +6,4 @@ pluginManagement {
 }
 
 rootProject.name = "Fusion"
-include("Fusion-API", "Fusion-Server")
+include("Fusion-API", "Fusion-Server", "spigot")
